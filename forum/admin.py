c3d7015e7318802b7d5d0148fb36c569f84da9a7
Alex.F <gfreezy@gmail.com>
2014-04-13 22:16:05 +0800

    后台加入用户

--- forum/admin.py
+++ forum/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from forum.models import Plane, Node, Blog
+from forum.models import Plane, Node, Blog, User
 
+admin.site.register(User)
 admin.site.register(Plane)
 admin.site.register(Node)
 admin.site.register(Blog)
